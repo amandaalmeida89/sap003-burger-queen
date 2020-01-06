00d5bcb4e40100f422eed08f22e664a2184cbf3b
amandaalmeida89 <amandaalmeida89@gmail.com>
2020-01-06 18:08:20 -0300

    implementacao da autenticacao firebase

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { useEffect } from "react";
 import {
-  BrowserRouter as Router, Switch, Route, useHistory,
+  BrowserRouter as Switch, Route, useHistory,
 } from "react-router-dom";
-import growl from "growl-alert";
 import firestore, { auth } from "./firebase.js";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
@@ -10,27 +9,19 @@ import Bartender from "./pages/Bartender";
 import Delivery from "./pages/Delivery";
 import Kitchen from "./pages/Kitchen";
 
-const option = {
-  fadeAway: true,
-  fadeAwayTimeout: 2000,
-};
-
 const App = () => {
   const history = useHistory();
 
   useEffect(() => {
     auth.onAuthStateChanged((user) => {
-      // console.log(user);
       if (user) {
         firestore.collection("users").doc(user.uid)
           .get().then((snap) => {
             const profileData = snap.data();
             console.log(profileData);
             if (profileData.service === "bartender") {
-              growl.success({ text: "Bem vindo!", ...option });
               history.push("/bartender");
             } else {
-              growl.success({ text: "Bem vindo!", ...option });
               history.push("/kitchen");
             }
           });
@@ -41,15 +32,13 @@ const App = () => {
   }, [history]);
 
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Login} />
-        <Route path="/register" component={Register} />
-        <Route path="/bartender" component={Bartender} />
-        <Route path="/delivery" component={Delivery} />
-        <Route path="/kitchen" component={Kitchen} />
-      </Switch>
-    </Router>
+    <Switch>
+      <Route exact path="/" component={Login} />
+      <Route path="/register" component={Register} />
+      <Route path="/bartender" component={Bartender} />
+      <Route path="/delivery" component={Delivery} />
+      <Route path="/kitchen" component={Kitchen} />
+    </Switch>
   );
 };
 
