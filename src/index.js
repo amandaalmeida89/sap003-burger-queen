85ca2f33f54441b115d6687b1bf3533ff6bf0ec5
amandaalmeida89 <amandaalmeida89@gmail.com>
2020-01-06 22:53:29 -0300

    exclusao arquivi auth

--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-
-// const app = React.createElement(App);
 ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById("root"));
 
-serviceWorker.unregister();
+serviceWorker.register();
